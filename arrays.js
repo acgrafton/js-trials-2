7d0343ac9f53c98b166613d42f3a24d085e3f8df
vagrant <vagrant@vagrant.vm>
2020-05-19 23:15:41 +0000

    Initial Commit

---
+++ arrays.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+// 1. printIndices
+// Print each item in the list, followed by its index. 
+function printIndices(items) {
+  for (const i in items) {
+    console.log(items[i], i)
+  } 
+}
+
+
+// 2. everyOtherItem
+function everyOtherItem(items) {
+  // Replace this with your code
+}
+
+
+// 3. smallestNItems
+function smallestNItems(items, n) {
+  // Replace this with your code
+}
