fb2ebdd8d6465d123856b3ac308f83e66e892ba7
vagrant <vagrant@vagrant.vm>
2020-05-19 23:40:52 +0000

    Completed Arrays.js

--- arrays.js
+++ arrays.js
@@ -3,6 +3,11 @@
 
 // 1. printIndices
 // Print each item in the list, followed by its index. 
+// Ex.:
+//    > printIndices(items)
+//    apple 0
+//    berry 1
+//    cherry 2
 function printIndices(items) {
   for (const i in items) {
     console.log(items[i], i)
@@ -11,12 +16,27 @@ function printIndices(items) {
 
 
 // 2. everyOtherItem
+// Print a list of every other item in 'items'
 function everyOtherItem(items) {
-  // Replace this with your code
+  const result = [];
+
+  for (const i in items){
+    if (i % 2 === 0){
+        result.push(items[i]);
+    }
+  }
+  console.log(result);    
 }
 
 
 // 3. smallestNItems
+//Print a list of the 'n' smallest integers in 'items'. 
 function smallestNItems(items, n) {
-  // Replace this with your code
+  
+  const sortedItems = items.sort((a,b) => a - b); 
+  const sortedNItems = sortedItems.slice(0, n);
+  sortedNItems.reverse();
+
+  console.log(sortedNItems)
+
 }
